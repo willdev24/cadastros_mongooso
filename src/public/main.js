bc3661d4cfbe9ccd9fc9924e87f0d9a24bb6960b
willy thiago <128657856+willdev24@users.noreply.github.com>
2024-08-15 21:14:08 -0400

    adiciona condiçao

--- src/public/main.js
+++ src/public/main.js
@@ -10,18 +10,24 @@ const main ={
     buscarhtml: function(){
     this.$excluir = document.querySelectorAll("#excluir")
     this.$alerta = document.querySelector(".alertaPai")    
-    },
+    this.$apagar = document.querySelector("#sim")
+    this.$retornar = document.querySelector("#nao")
 
+},
 
     AdicionarEventos:function(){
         const self = this
         
         this.$excluir.forEach( itens => {
-            itens.addEventListener('click',self.Evento.adicionarClick.bind(self))
-            
+            itens.addEventListener('click',self.Evento.adicionarClick.bind(self))            
         });
 
-                
+        this.$apagar.addEventListener('click',self.Evento.Prosseguir.bind(self))
+        
+        this.$retornar.addEventListener('click',self.Evento.retornar.bind(self))
+    
+
+
     },
 
 
@@ -35,6 +41,18 @@ if(done == false){
     this.$alerta.classList.add("abrir")
 
 }
+
+},
+
+Prosseguir: function(e){
+
+
+},
+
+retornar:function(e){
+
+    window.location.href = window.location.origin + "/listar"
+        
 }
 
 
